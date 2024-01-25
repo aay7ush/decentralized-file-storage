186cbf317203874e61dbdf7e87330ef5713f8696
Aayush Jaiswal <aay7ush@gmail.com>
2024-01-25 15:52:33 +0530

    refactor: remove unnecessary tailwind classes

--- components/MainContent.tsx
+++ components/MainContent.tsx
@@ -8,7 +8,7 @@ const MainContent = () => {
   const [files, setFiles] = useState<FileObject[]>([])
 
   return (
-    <div className="min-h-screen place-content-center grid grid-cols-1 md:grid-cols-2 gap-7 px-5 md:px-7 my-10 md:my-0">
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-7 p-5">
       <DragFiles setFiles={setFiles} />
       <DisplayFiles files={files} setFiles={setFiles} />
     </div>
