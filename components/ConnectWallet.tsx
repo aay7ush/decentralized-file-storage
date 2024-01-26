9613d25087fd1ff816092ea8792e4440b55229fd
charans29 <smodugula@csuchico.edu>
2024-01-25 19:26:04 -0800

    Added Wallet Selector

--- components/ConnectWallet.tsx
+++ components/ConnectWallet.tsx
@@ -1,22 +1,29 @@
 "use client"
-
-import { useMbWallet } from "@mintbase-js/react"
+import { useMbWallet } from "@mintbase-js/react";
 import { Button } from "./ui/button"
 
-export function ConnectWallet() {
-  const { connect, disconnect, isConnected } = useMbWallet()
+export const ConnectWallet = () => {
+  const { isConnected, selector, connect , activeAccountId } = useMbWallet();
+
+  const handleSignout = async () => {
+    const wallet = await selector.wallet();
+    return wallet.signOut();
+  };
+
+  const handleSignIn = async () => {
+    return connect();
+  };
+
+  if (!isConnected) {
+    return <Button onClick={handleSignIn}>Connect To NEAR</Button>;
+  }
 
   return (
-    <Button
-      onClick={() => {
-        if (isConnected) {
-          disconnect()
-        } else {
-          connect()
-        }
-      }}
-    >
-      {isConnected ? "Disconnect" : "Connect"}
-    </Button>
-  )
-}
+    <div>
+      {/* <p>You are connected as {activeAccountId}</p> */}
+      <div>
+        <Button onClick={handleSignout}> Disconnect </Button>
+      </div>
+    </div>
+  );
+};
