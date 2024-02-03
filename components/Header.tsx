6310cc051814f92335da778c06cdd73eb85510ff
Subin Khatiwada <Subinkhatiwada@gmail.com>
2024-02-03 17:42:08 +0200

    feat: add balance status

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,17 @@
-import Link from "next/link"
-import { ConnectWallet } from "./ConnectWallet"
+import Link from "next/link";
+import { ConnectWallet } from "./ConnectWallet";
+import ShowBalance from "./ShowBalance";
 
 const Header = () => {
+  const userAccountId = "subin123.testnet";
   return (
     <header className="flex justify-between items-center p-5">
       <Link href="/" className="text-2xl font-bold">
         DApp File Storage
       </Link>
+      <ShowBalance accountId={userAccountId} />
       <ConnectWallet />
     </header>
-  )
-}
-export default Header
+  );
+};
+export default Header;
