5b8fa3461a87dc3a942cd2ca9493256bcb5f348d
Aayush Jaiswal <aay7ush@gmail.com>
2024-02-04 23:15:15 +0530

    fix: transaction error

--- lib/nearContract.ts
+++ lib/nearContract.ts
@@ -1,5 +1,5 @@
 import nearConfig from "@/lib/nearConfig"
-import { Account, ConnectConfig, KeyPair, connect, utils } from "near-api-js"
+import { Account, ConnectConfig, connect } from "near-api-js"
 
 export default async function deductNearEquivalentToOneDollar(
   activeAccountId: string,
@@ -9,14 +9,14 @@ export default async function deductNearEquivalentToOneDollar(
   const account = new Account(near.connection, activeAccountId)
   const oneDollarInYoctoNEAR = "100000000000000000000000"
 
-  const newKeyPair = KeyPair.fromRandom("ed25519")
+  // const newKeyPair = KeyPair.fromRandom("ed25519")
 
-  await account.addKey(
-    newKeyPair.publicKey,
-    null,
-    null,
-    utils.format.parseNearAmount("1")
-  )
+  // await account.addKey(
+  //   newKeyPair.getPublicKey(),
+  //   null,
+  //   null,
+  //   utils.format.parseNearAmount("1")
+  // )
 
   await account.sendMoney(recipientId, oneDollarInYoctoNEAR)
   console.log("Money sent successfully")
