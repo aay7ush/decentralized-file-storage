c28cf2d887e12e1c371cc70e50c2918f4b603f8d
Adesh Kumar Patra <adeshkpatra@akpsmac-3.local>
2024-02-05 00:40:03 +0530

    ui footer fixed

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import MainContent from "@/components/MainContent"
 
 export default function Home() {
   return (
-    <main>
+    <main className="min-h-[calc(90vh-64px)] flex items-center">
       <MainContent />
     </main>
   )
